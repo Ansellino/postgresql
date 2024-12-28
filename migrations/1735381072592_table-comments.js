d95e72e598f16d18034e6b5f8562cc8232f9d60f
AnselZorro <ansellino@gmail.com>
2024-12-28 17:28:42 +0700

    add  create table comments and drop table comments in migration

--- migrations/1735381072592_table-comments.js
+++ migrations/1735381072592_table-comments.js
@@ -8,11 +8,24 @@ exports.shorthands = undefined;
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-exports.up = (pgm) => {};
+exports.up = (pgm) => {
+    pgm.sql(`
+        CREATE TABLE comments (
+            id SERIAL PRIMARY KEY,
+            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+            contents VARCHAR(240) NOT NULL
+        );
+    `);
+};
 
 /**
  * @param pgm {import('node-pg-migrate').MigrationBuilder}
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-exports.down = (pgm) => {};
+exports.down = (pgm) => {
+    pgm.sql(`
+        DROP TABLE comments;
+    `);
+};
